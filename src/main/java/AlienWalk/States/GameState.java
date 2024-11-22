de559a4103ed5b5d4ebe39f52111a9c6e954b0c7
Jakub <kuba.jankowski345@gmial.com>
2024-11-22 21:11:51 +0000

    minor change

--- src/main/java/AlienWalk/States/GameState.java
+++ src/main/java/AlienWalk/States/GameState.java
@@ -4,6 +4,10 @@ import AlienWalk.Controller.GameController;
 import AlienWalk.Game;
 import AlienWalk.Model.Level;
 import AlienWalk.Viewer.GameViewer;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+
+import java.io.IOException;
 
 public class GameState extends State<Level>{
     public GameState(Level level, GameViewer gameViewer, GameController gameController){
@@ -12,6 +16,15 @@ public class GameState extends State<Level>{
 
     @Override
     public void step(Game game) {
+        viewer.draw(model);
+        KeyStroke key = viewer.read();
+        if (key.getKeyType() == KeyType.EOF) {
+            game.state = null;
+        }
+        try {
+            controller.processInput(key, game, model);
+        }
+        catch (IOException ignored) {}
 
     }
 }
